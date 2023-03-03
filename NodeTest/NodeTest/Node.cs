a681e8c8afde4e70a9f30f6af1ce8757ec0d999b
GlitterMakesGames <123193351+GlitterMakesGames@users.noreply.github.com>
2023-03-03 08:26:36 -0600

    Add files via upload

---
+++ NodeTest/NodeTest/Node.cs
@@ -0,0 +1,27 @@
+﻿class Node
+{
+    private Node? _parent;
+    private Node? _leftChild;
+    private Node? _rightChild;
+    private int _priority;
+
+    public Node parent { get => get(in _parent); set => set(out _parent, in value); }
+    public Node leftChild { get => get(in _leftChild); set => set(out _leftChild, in value); }
+    public Node rightChild { get => get(in _rightChild); set => set(out _rightChild, in value); }
+    public int priority { get => this._priority; set => _priority = value; }
+
+    public Node(int p)
+    {
+        _priority = p;
+    }
+
+    public Node get(in Node getBacking)
+    {
+        return getBacking;
+    }
+
+    public void set(out Node setBacking, in Node newRef)
+    {
+        setBacking = newRef;
+    }
+}
---
+++ NodeTest/NodeTest/NodeTest.cs
@@ -0,0 +1,21 @@
+﻿class NodeTest
+{
+    public static void Main(string[] args)
+    {
+        Node root = new Node(2);
+        Node leaf1 = new Node(1);
+        Node leaf2 = new Node(3);
+
+        root.leftChild = leaf1;
+        root.rightChild = leaf2;
+        leaf1.parent = root;
+        leaf2.parent = root;
+
+        Node current = root;
+        Console.WriteLine("The priority of the root is " + current.priority);
+        current = current.leftChild;
+        Console.WriteLine("The priority of the leftmost leaf is " + current.priority);
+        current = current.parent.rightChild;
+        Console.WriteLine("The priority of the rightmost leaf is "+ current.priority);
+    }
+}
